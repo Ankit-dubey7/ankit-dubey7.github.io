ed5d3c6af9293d2b21130b9d1e8de967a3228189
Ankit-dubey7 <Ankitdubey25122001@gmail.com>
2026-06-28 23:57:04 +0530

    Implement dark mode toggle and scroll-to-top feature
    Added functionality for dark mode toggle and scroll-to-top button.

--- script.js
+++ script.js
@@ -17,3 +17,52 @@ window.addEventListener("scroll", () => {
     });
 
 });
+// Dark Mode
+
+const themeBtn = document.getElementById("theme-toggle");
+
+themeBtn.addEventListener("click",()=>{
+
+document.body.classList.toggle("light");
+
+if(document.body.classList.contains("light")){
+
+themeBtn.innerHTML="☀️";
+
+}else{
+
+themeBtn.innerHTML="🌙";
+
+}
+
+});
+
+// Scroll To Top
+
+const topBtn=document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.addEventListener("click",()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+});
